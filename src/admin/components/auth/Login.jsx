9f3699a6f4babc5a49cda4a5e0e0984228a2a63b
Muhammad Shayan <shayanarif666@gmail.com>
2025-02-19 10:53:33 +0500

    Change URL

--- src/admin/components/auth/Login.jsx
+++ src/admin/components/auth/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     const loginAccount = async (data) => {
         setLoading(true)
         try {
-            const response = await fetch(`http://localhost:5000/api/auth/login`, {
+            const response = await fetch(`https://skynetsilicon-website-backend.vercel.app/api/auth/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data)
--- src/admin/components/auth/Register.jsx
+++ src/admin/components/auth/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
     const registerAccount = async (data) => {
         setLoading(true)
         try {
-            const response = await fetch(`http://localhost:5000/api/auth/register`, {
+            const response = await fetch(`https://skynetsilicon-website-backend.vercel.app/api/auth/register`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data)
